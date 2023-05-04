ea0426f953c14be30b61dbaa80cfc0d3b2f511bb
Yahir-Puebla <117696219+Yahir-Puebla@users.noreply.github.com>
2023-05-04 16:30:52 -0600

    Largue Cactus Agregated

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,11 +1,23 @@
 import random
 from dino_runner.components.obstacles.obstacle import Obstacle
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 class Cactus(Obstacle):
-    Y_POS_CACTUS = 325
+    Y_POS_CACTUS_SMALL= 325
+    Y_POS_CACTUS_LARGE = 325
+    COMBINED_CACTUS = []
+
+
 
     def __init__(self):
-        self.image = random.choice(SMALL_CACTUS)
+        self.COMBINED_CACTUS = (SMALL_CACTUS + LARGE_CACTUS)
+        self.image = random.choice(self.COMBINED_CACTUS)
+
         super().__init__(self.image)
-        self.rect.y = self.Y_POS_CACTUS
+
+        self.rect.y = self.Y_POS_CACTUS_SMALL
+
+        if self.image in SMALL_CACTUS:
+            self.rect.y = self.Y_POS_CACTUS_SMALL
+        else:
+            self.rect.y = self.Y_POS_CACTUS_LARGE
