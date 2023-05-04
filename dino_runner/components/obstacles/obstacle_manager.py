179b5812e50ae335d63198a18c97f275b85cf442
Yahir-Puebla <117696219+Yahir-Puebla@users.noreply.github.com>
2023-05-04 07:40:39 -0600

    Avanse en pajaros

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,4 +1,5 @@
 from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
@@ -8,6 +9,7 @@ class ObstacleManager:
     def update(self, game_speed, player):
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus())
+            self.obstacles.append(Bird())
         for obstacle in self.obstacles:
             if obstacle.rect.x < -obstacle.rect.width:
                 self.obstacles.remove(obstacle)
